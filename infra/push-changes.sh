701910bd174f04217dc5bd53122de268d64e946b
Will Verland <will@ver.land>
2024-03-02 16:05:27 +1300

    Tweak infra scripts

--- infra/push-changes.sh
+++ infra/push-changes.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+set -e
+
+docker build -t url-shortener .
+docker tag url-shortener ghcr.io/un1r8okq/url-shortener:LATEST
 echo GITHUB_TOKEN | docker login ghcr.io -u un1r8okq --password-stdin
 docker push ghcr.io/un1r8okq/url-shortener:LATEST
