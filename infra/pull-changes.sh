d93cc01d9fe5baa5c3860a6502acfe6d22159458
Will Verland <will@ver.land>
2024-03-03 16:55:26 +1300

    Fix using specific version

--- infra/pull-changes.sh
+++ infra/pull-changes.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -e
 
-$version=$(cat version.txt)
-
-docker pull ghcr.io/un1r8okq/url-shortener:$version
-docker compose up --detach --env VERSION=$version
+docker compose --env-file version.txt --env-file .env up --detach
--- infra/push-changes.sh
+++ infra/push-changes.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 
-$version=$(cat version.txt)
+export $(cat .env | xargs)
+export $(cat version.txt | xargs)
 
 docker build -t url-shortener .
-docker tag url-shortener ghcr.io/un1r8okq/url-shortener:$version
-echo GITHUB_TOKEN | docker login ghcr.io -u un1r8okq --password-stdin
-docker push ghcr.io/un1r8okq/url-shortener:$version
+docker tag url-shortener ghcr.io/un1r8okq/url-shortener:$VERSION
+echo $GITHUB_TOKEN | docker login ghcr.io -u un1r8okq --password-stdin
+docker push ghcr.io/un1r8okq/url-shortener:$VERSION
